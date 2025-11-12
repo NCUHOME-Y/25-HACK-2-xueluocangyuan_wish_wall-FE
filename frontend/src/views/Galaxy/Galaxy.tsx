40a1e722b33d5e381e7b2c011ec349e579b55ac4
Jane <1500532940@qq.com>
2025-11-12 14:40:08 +0800

    feat: 个人页面

--- frontend/src/views/Galaxy/Galaxy.tsx
+++ frontend/src/views/Galaxy/Galaxy.tsx
@@ -1,9 +1,23 @@
- 
+ import BackButton from "@/components/common/BackButton.tsx";
+ import ProfileButton from "@/components/common/ProfileButton.tsx";
+ import snowflakeImg from "@/assets/images/雪花.svg";
+ import "@/styles/Galaxy.css"
 
 function Galaxy() {
   return (
-    <div>
-      <h1>个人心愿</h1>
+    <div className="wish-list-container">
+      <div className="header">
+      <BackButton />
+      <h2 className="title">小雪心愿单</h2>
+      <ProfileButton />
+      </div>
+      <div className="wish-list">
+        <div className="wish-item">
+        <img src={snowflakeImg} alt="雪花" />
+        {/* 请雨木木替换为实际数据 */}
+        <span className="wish-content">何意味何意味何意味何意味</span>
+      </div>
+      </div>
     </div>
   );
 }
