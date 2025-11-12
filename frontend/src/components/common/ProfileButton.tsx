672bb710cec31f148c66f18426926fe075190e46
Jane <1500532940@qq.com>
2025-11-12 20:28:45 +0800

    feat: back-button及profile-button

---
+++ frontend/src/components/common/ProfileButton.tsx
@@ -0,0 +1,20 @@
+import { useNavigate } from'react-router-dom';
+import Button from './Button.tsx';
+
+const ProfileButton = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/Profile');
+  };
+
+  return (
+    <Button 
+      onClick={handleClick}
+      className="profile-button"
+      icon="" 
+    />
+  );
+};
+
+export default ProfileButton;
